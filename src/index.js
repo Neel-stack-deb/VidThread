4e8b42b1fdaca289cd6bfdd4337ed9308a3c06a7
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-08-03 21:40:02 +0530

    fixed some package realted errors and imported some nessesary modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { connectToDatabase } from "./db/index.js";
-
+import {app} from "./app.js";
+import dotenv from 'dotenv';
+dotenv.config();
+ 
 const port = process.env.PORT || 3000;
 
 connectToDatabase()
