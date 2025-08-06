d4df8e217e8b259cce8452f6790207bd07b64d9c
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-08-06 09:52:28 +0530

    added the fields in the models and also used encryption middleware and added a password validator in the Schema methods.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,16 +1,60 @@
-import { mongoose, Schema } from "mongoose";
-
+import { mongoose, Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 const userSchema = new Schema({
-  
-
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    index: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    index: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  }, 
+  avatar: {
+    type: String,
+    required: true
+  },
+  coverImage: {
+    type: String
+  },  
+  watchHistory: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Video'
+  }],
+  refreshToken: {
+    type: String,
+  }
 },{
-
+  timestamps: true,
 });
 
-userSchema.methods();
-
-userSchema.pre('save',function(){
+userSchema.methods.comparePassword(async function(password){
+  return await bcrypt.compare(password, this.password)
   
+});
+
+userSchema.pre('save',async function (next){
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,10 +1,45 @@
-import { mongoose, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
 const videoSchema = new Schema({
-
-
+  videoFile: {
+    type: String, 
+    required: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  views: {
+    type: Number,
+    default: 0
+  },
+  isPublic: {
+    type: Boolean,
+    default: true
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }  
 },{
-
+  timestamps: true
 });
 
 
