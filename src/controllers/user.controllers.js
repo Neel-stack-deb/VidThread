939080ebf5f43afe47b05a08d4c913c7a384318f
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-08-10 23:55:11 +0530

    planning to registering a user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,15 +1,12 @@
 import { asyncHandler } from '../utils/asyncHandler.js';
 
 export const registerUser = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
-
-  // Here you would typically handle user registration logic, such as saving the user to a database.
-  // For demonstration purposes, we'll just send a success response.
-
-  if (!username || !password) {
-    return res.status(400).json({ message: 'Username and password are required.' });
-  }
-
-  // Simulate user registration success
-  res.status(201).json({ message: 'User registered successfully', user: { username } });
+  //First we will take the data fro the frontend
+  // Then we will validate the data
+  //then we will chack if the user already exists
+  //check for images and avater (all the compuslary fields)
+  //upload the image to the claudinary
+  //then we will create the user in the database
+  //then we will send the response to the frontend
+  
 });  
