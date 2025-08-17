dfef36bd27b3bc41e2b8891fd77b49d22f9ad595
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2025-08-17 17:41:13 +0530

    feat: add routes for updating user avatar, cover photo, and profile information

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import { loginUser, registerUser, logoutUser, refreshAccessToken, updateUserAvatar, updateUserCoverPhoto, updateUserProfile } from "../controllers/user.controllers.js";
 import { uploads } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { use } from "react";
 const userRouter=new Router();
 
 userRouter.route("/register").post(uploads.fields([
@@ -15,5 +16,14 @@ userRouter.route("Token-Refresh").post(refreshAccessToken);
 
 // secured route
 userRouter.route("/logout").post(verifyJWT, logoutUser);
+
+// This route is for updating the user's avatar
+userRouter.route("/updateAvatar").put(verifyJWT, uploads.single('avatar'), updateUserAvatar);
+  
+// This route is for updating the user's cover photo
+userRouter.route("/updateCoverPhoto").put(verifyJWT, uploads.single('coverPhoto'), updateUserCoverPhoto);
+
+// This route is for updating the user's profile information
+userRouter.route("/updateProfile").put(verifyJWT, updateUserProfile);
 
 export { userRouter };
